8b9ee2ac1610aa52828fbbd2902da847d75d9bb5
Brett DeWitt <bdewitt1984@gmail.com>
2024-07-08 15:22:04 -0700

    Updated unit tests for command.MoveVideo to accommodate the function rewrite.

--- tests/test_command.py
+++ tests/test_command.py
@@ -41,11 +41,12 @@ class TestMoveVideoCommand(unittest.TestCase):
         self.cmd = MoveVideo(self.vid, self.dest_dir)
 
     def setup_undo(self):
-        pre_undo_path = os.path.join(self.dest_dir, self.filename)
-        self.moved_path = shutil.move(self.test_file.name, pre_undo_path)
+        self.cmd.undo_dir = self.src_dir.name
+        self.cmd.created_dirs.append(self.dest_dir)
+        self.moved_path = shutil.move(self.test_file.name, self.dest_dir)
         self.vid.data = {
             "file_data": {
-                "path": pre_undo_path,
+                "path": self.moved_path,
                 "root": self.dest_dir,
                 "filename": self.filename
             }
@@ -53,81 +54,6 @@ class TestMoveVideoCommand(unittest.TestCase):
 
     def tearDown(self) -> None:
         self.src_dir.cleanup()
-
-    def test_validate_exec_valid(self):
-        self.cmd.validate_exec()
-
-    def test_validate_exec_invalid_src(self):
-        os.remove(self.test_file.name)
-        cmd = MoveVideo(self.vid, self.dest_dir)
-        with self.assertRaises(ValidationError):
-            cmd._validate_file_exists(cmd.src_file_path)
-        with self.assertRaises(ValidationError):
-            cmd.validate_exec()
-
-    def test_validate_exec_dest_already_exists(self):
-        dest_file_path = os.path.join(self.dest_dir, self.filename)
-        temp_file = open(dest_file_path, 'w')
-        temp_file.close()
-        with self.assertRaises(ValidationError):
-            self.cmd._validate_file_not_exist(self.cmd.dest_file_path)
-        with self.assertRaises(ValidationError):
-            self.cmd.validate_exec()
-
-    def test_validate_exec_path_has_changed(self):
-        dest = os.path.join(self.src_dir.name, 'new_folder')
-        os.makedirs(dest)
-        new_path = shutil.move(self.test_file.name, dest)
-        new_root, new_filename = os.path.split(new_path)
-
-        self.vid.data = {
-            "file_data": {
-                "path": new_path,
-                "root": new_root,
-                "filename": new_filename
-            }
-        }
-
-        with self.assertRaises(ValidationError):
-            self.cmd._validate_path_not_changed(self.cmd.src_file_path)
-        with self.assertRaises(ValidationError):
-            self.cmd.validate_exec()
-
-    def test_validate_undo_valid(self):
-        self.setup_undo()
-        self.cmd.undo()
-
-    def test_validate_undo_invalid_src(self):
-        self.setup_undo()
-        os.remove(self.moved_path)
-        with self.assertRaises(ValidationError):
-            self.cmd._validate_file_exists(self.cmd.dest_file_path)
-
-    def test_validate_undo_dest_already_exists(self):
-        self.setup_undo()
-        temp_file = open(self.src_file_path, 'w')
-        temp_file.close()
-        with self.assertRaises(ValidationError):
-            self.cmd._validate_file_not_exist(self.cmd.src_file_path)
-
-    def test_validate_undo_path_has_changed(self):
-        self.setup_undo()
-
-        dest = os.path.join(self.src_dir.name, 'new_folder')
-        os.makedirs(dest)
-        new_path = shutil.move(self.moved_path, dest)
-        new_root, new_filename = os.path.split(new_path)
-
-        self.vid.data = {
-            "file_data": {
-                "path": new_path,
-                "root": new_root,
-                "filename": new_filename
-            }
-        }
-
-        with self.assertRaises(ValidationError):
-            self.cmd._validate_path_not_changed(self.cmd.dest_file_path)
 
     def test_exec(self):
         expected_path = os.path.join(self.dest_dir, self.filename)
@@ -142,3 +68,4 @@ class TestMoveVideoCommand(unittest.TestCase):
         self.cmd.undo()
         self.assertTrue(os.path.exists(expected_file_path))
         self.assertEqual(expected_file_path, self.vid.get_path())
+        self.assertFalse(os.path.exists(self.dest_dir))
