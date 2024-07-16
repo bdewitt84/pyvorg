56a22223a47d9c7a853b1c11b9ccaa3819f3fbe1
Brett DeWitt <bdewitt1984@gmail.com>
2024-07-15 19:47:53 -0700

    Added abstract methods get_name(), get_optional_params(), and get_required_params() to the base class.

--- source/api/api_interface.py
+++ source/api/api_interface.py
@@ -9,6 +9,17 @@ class APIInterface(ABC):
         pass
 
     @abstractmethod
-    def fetch_video_data(self, title):
-        raise NotImplementedError("This function must be called from a subclass of APIInterface, not APIInterface "
-                                  "itself.")
+    def fetch_video_data(self, **kwargs):
+        raise NotImplementedError("This function must be implemented in a subclass")
+
+    @abstractmethod
+    def get_name(self):
+        raise NotImplementedError("This function must be implemented in a subclass")
+
+    @abstractmethod
+    def get_optional_params(self):
+        raise NotImplementedError("This function must be implemented in a subclass")
+
+    @abstractmethod
+    def get_required_params(self):
+        raise NotImplementedError("This function must be implemented in a subclass")
