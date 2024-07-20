2aa77135b13417f038a2151c5bd5b1b486396ee9
Brett DeWitt <bdewitt1984@gmail.com>
2024-07-19 20:29:30 -0700

    Renamed for clarity, added parameter 'name', and un-abstracted get_name() for simplicity.

--- source/api/base_api.py
+++ source/api/base_api.py
@@ -1,20 +1,20 @@
-# source/api/api_interface.py
+# source/api/base_api.py
 
 # Standard library
 from abc import ABC, abstractmethod
 
 
-class APIInterface(ABC):
-    def __init__(self):
-        pass
+# class BaseAPI(ABC, metaclass=MetaAPI):
+class BaseAPI(ABC):
+    def __init__(self, name=None):
+        self.name = name
 
     @abstractmethod
     def fetch_video_data(self, **kwargs):
         raise NotImplementedError("This function must be implemented in a subclass")
 
-    @abstractmethod
     def get_name(self):
-        raise NotImplementedError("This function must be implemented in a subclass")
+        return self.name
 
     @abstractmethod
     def get_optional_params(self):
