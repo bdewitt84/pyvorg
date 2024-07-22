cdd96c434e32af902f261f5490038892b9d41b92
Brett DeWitt <bdewitt1984@gmail.com>
2024-07-22 14:32:40 -0700

    Fixed error that was occurring across tests; Python caches imports, so any test that imported our simulated package beyond the first test's import. This is incorrect, since that package's __path__ member points to a temporary directory that gets torn down after the test is completed. To resolve the error, we call importlib.reload() to force Python to import the package from the current temporary directory.

--- tests/test_api/test_api_manager.py
+++ tests/test_api/test_api_manager.py
@@ -1,11 +1,17 @@
 # /tests/api/api_manager.py
+Fixed
+"""
+    Unit tests for ./source/api/api_manager.py
+"""
+
+# Standard library
 import importlib
 import os
-# Standard library
+import sys
 from tempfile import TemporaryDirectory
 from unittest import TestCase
-from unittest.mock import MagicMock, patch
-import sys
+from unittest.mock import MagicMock
+
 
 # Local imports
 from source.api.api_manager import APIManager
@@ -61,13 +67,39 @@ class TestAPIManager(TestCase):
         self.test_api = TestAPI()
         self.temp_dir = TemporaryDirectory()
 
+        # Simulated package
+        self.fake_pkg_name = 'fake_pkg'
+        fake_pkg_path = os.path.join(self.temp_dir.name, self.fake_pkg_name)
+        self.temp_path = fake_pkg_path
+        os.mkdir(fake_pkg_path)
+        fake_pkg_init_path = os.path.join(fake_pkg_path, '__init__.py')
+        with open(fake_pkg_init_path, 'w') as file:
+            file.write('# This file makes the directory a package.')
+
+        # Simulate modules
+        self.fake_mod_1_name = 'fake_api_1'
+        self.fake_mod_2_name = 'fake_api_2'
+        fake_mod_1_path = os.path.join(fake_pkg_path, self.fake_mod_1_name + '.py')
+        fake_mod_2_path = os.path.join(fake_pkg_path, self.fake_mod_2_name + '.py')
+        with open(fake_mod_1_path, 'w') as file:
+            file.write(
+                """from source.api.base_api import BaseAPI\nclass FakeAPI1(BaseAPI):\n    def fetch_video_data(self, **kwargs):\n        pass\n    def get_optional_params(self):\n        pass\n    def get_required_params(self):\n        pass""")
+        with open(fake_mod_2_path, 'w') as file:
+            file.write(
+                """from source.api.base_api import BaseAPI\nclass FakeAPI2(BaseAPI):\n    def fetch_video_data(self, **kwargs):\n        pass\n    def get_optional_params(self):\n        pass\n    def get_required_params(self):\n        pass""")
+
+        # Prepare path for imports
+        sys.path.insert(0, self.temp_dir.name)
+
     def tearDown(self) -> None:
         self.temp_dir.cleanup()
+        sys.path.pop(0)
 
     def test_discover_api_classes(self):
 
         # Arrange
         fake_api_mod = MagicMock()
+        fake_api_mod.BaseAPI = BaseAPI
         fake_api_mod.FakeAPI1 = FakeAPI1
         fake_api_mod.FakeAPI2 = FakeAPI2
 
@@ -82,39 +114,21 @@ class TestAPIManager(TestCase):
         self.assertEqual(expected_value, result)
 
     def test_discover_api_modules(self):
-        # Arrange - simulate package
-        fake_pkg_name = 'fake_pkg'
-        fake_pkg_path = os.path.join(self.temp_dir.name, fake_pkg_name)
-        os.mkdir(fake_pkg_path)
-        fake_pkg_init_path = os.path.join(fake_pkg_path, '__init__.py')
-        with open(fake_pkg_init_path, 'w') as file:
-            file.write('# This file makes the directory a package.')
 
-        # Arrange - simulate modules
-        fake_mod_1_name = 'fake_api_1'
-        fake_mod_2_name = 'fake_api_2'
-        fake_mod_1_path = os.path.join(fake_pkg_path, fake_mod_1_name + '.py')
-        fake_mod_2_path = os.path.join(fake_pkg_path, fake_mod_2_name + '.py')
-        with open(fake_mod_1_path, 'w') as file:
-            file.write(
-                """from source.api.base_api import BaseAPI\nclass FakeAPI1(BaseAPI):\n    def fetch_video_data(self, **kwargs):\n        pass\n    def get_optional_params(self):\n        pass\n    def get_required_params(self):\n        pass""")
-        with open(fake_mod_2_path, 'w') as file:
-            file.write(
-                """from source.api.base_api import BaseAPI\nclass FakeAPI2(BaseAPI):\n    def fetch_video_data(self, **kwargs):\n        pass\n    def get_optional_params(self):\n        pass\n    def get_required_params(self):\n        pass""")
-
-        # Arrange - import simulated package
-        sys.path.insert(0, self.temp_dir.name)
-        test_pkg = importlib.import_module(fake_pkg_name)
+        # Arrange
+        test_pkg = importlib.import_module(self.fake_pkg_name)
+        importlib.reload(test_pkg)  # Avoids using cache with outdated path from other unit tests
 
         # Act
         result = self.test_api_man.discover_api_modules(test_pkg)
 
         # Assert
-        expected_value = {f'{fake_pkg_name}.{fake_mod_1_name}', f'{fake_pkg_name}.{fake_mod_2_name}'}
+        expected_value = {
+            f'{self.fake_pkg_name}.{self.fake_mod_1_name}',
+            f'{self.fake_pkg_name}.{self.fake_mod_2_name}'
+        }
         self.assertEqual(expected_value, set(result.keys()))
         self.assertTrue(all(mod.__name__ in expected_value for mod in result.values()))
-
-        sys.path.pop(0)
 
     def test_get_api_list(self):
         # Arrange
@@ -137,6 +151,20 @@ class TestAPIManager(TestCase):
         # Assert
         expected_value = ['test_name']
         self.assertEqual(expected_value, result)
+
+    def test_init_plugins(self):
+        # Arrange
+        test_pkg = importlib.import_module(self.fake_pkg_name)
+        importlib.reload(test_pkg)  # Avoids using cache with outdated path from other unit tests
+
+        # Act
+        self.test_api_man.init_plugins(test_pkg)
+
+        # Assert
+        self.assertTrue(FakeAPI1.__name__ in self.test_api_man.apis)
+        self.assertTrue(FakeAPI2.__name__ in self.test_api_man.apis)
+        self.assertTrue(isinstance(self.test_api_man.apis.get(FakeAPI1.__name__), test_pkg.fake_api_1.FakeAPI1))
+        self.assertTrue(isinstance(self.test_api_man.apis.get(FakeAPI2.__name__), test_pkg.fake_api_2.FakeAPI2))
 
     def test_register_api(self):
         # Arrange
