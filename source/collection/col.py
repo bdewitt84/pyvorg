bf7cf9781f22166fe9b029e02f2639d869d1b4bd
Brett DeWitt <bdewitt1984@gmail.com>
2024-07-28 14:11:05 -0700

    Added serialization functions from_dict(), from_json(), to_dict(), to_json(), refactored self.cb to SessionManager

--- source/collection/col.py
+++ source/collection/col.py
@@ -12,13 +12,13 @@ import logging
 import os
 
 # Local imports
-from source.api.api_manager import APIManager
 from source.collection.video import Video
-from source.command.combuffer import CommandBuffer
 from source.constants import *
 from source.utils.helper import default_serializer
 from source.utils.helper import file_write
 
+# Third-party packages
+# N/A
 
 # TODO: Moving files needs to take associated subtitles, at the very least.
 #       consider adding a function that searches for the files with an
@@ -29,11 +29,10 @@ from source.utils.helper import file_write
 class Collection:
     def __init__(self, path=None):
         self.videos = {}
-        self.cb = CommandBuffer()
-        if path:
-            self.metadata_load(path)
-        self.api_manager = APIManager()
-        self.api_manager.init_plugins()
+        if path is not None:
+            self.path = path
+        else:
+            self.path = os.getenv('SOURCE_PATH')
 
     def add_video(self, path):
         video = Video()
@@ -42,8 +41,22 @@ class Collection:
         # TODO: logging output conflicts with tqdm, output is messy
         logging.info(f"Added '{path}' to collection")
 
+    @staticmethod
+    def from_dict(d):
+        # Todo: Validate input dictionary
+        new = Collection()
+        for hash, dict in d:
+            new.videos.update({hash, Video.from_dict(dict)})
+
+    @staticmethod
+    def from_json(j):
+        return Collection.from_dict(json.loads(j))
+
     def get_video(self, hash):
         return self.videos.get(hash)
+
+    def get_videos(self):
+        return self.videos.values()
 
     def metadata_load(self, path="./metadata.json"):
         with open(path, 'r') as file:
@@ -57,20 +70,20 @@ class Collection:
     def metadata_save(self, path="./metadata.json"):
         file_write(path, self.to_json())
 
-    def organize_files(self, dest):
-        # TODO: This ought to create (and return?) a transaction (command buffer)
-        #       Then we give the user a chance to preview it
-        #       Finally the user may execute the transaction
-
-        cb = CommandBuffer()
-        os.makedirs(dest, exist_ok=True)
-        for video in self.videos.values():
-            vid_dest = os.path.join(dest, video.generate_dir_name())
-            cb.add_move_video(video, vid_dest)
-        cb.preview_buffer()
-        cb.validate_exec_buffer()
-        # get user confirmation
-        # cb.execute_cmd_buffer()
+    # def organize_files(self, dest):
+    #     # TODO: This ought to create (and return?) a transaction (command buffer)
+    #     #       Then we give the user a chance to preview it
+    #     #       Finally the user may execute the transaction
+    #
+    #     cb = CommandBuffer()
+    #     os.makedirs(dest, exist_ok=True)
+    #     for video in self.videos.values():
+    #         vid_dest = os.path.join(dest, video.generate_dir_name())
+    #         cb.add_move_video(video, vid_dest)
+    #     cb.preview_buffer()
+    #     cb.validate_exec_buffer()
+    #     # get user confirmation
+    #     # cb.execute_cmd_buffer()
 
     def scan_directory(self, path):
         # TODO: standardize use of path, filename, and root or dir
@@ -80,20 +93,27 @@ class Collection:
                     file_path = os.path.join(root, file)
                     self.add_video(file_path)
 
+    def to_dict(self):
+        return {
+            hash: video.data
+            for hash, video
+            in self.videos.items()
+        }
+
     def to_json(self):
-        return json.dumps({hash: video.data for hash, video in self.videos.items()},
+        return json.dumps(self.to_dict(),
                           indent=4,
                           skipkeys=True,
                           default=default_serializer)
 
-    def update_api_data(self, **kwargs):
+    def update_api_data(self, api, **kwargs):
         """
-        Updates all video data using every API currently registered with
-        the current instance of APIManager stored in self.api_manager
+        Updates all videos with data fetched from supplied API plugin
 
+        :param api: API plugin to fetch data
         :param kwargs: Keyword arguments to pass to API plugin
         :return: None
         """
+
         for video in self.videos.values():
-            for api in self.api_manager.apis.values():
-                video.update_api_data(api, **kwargs)
+            video.update_api_data(api, **kwargs)
