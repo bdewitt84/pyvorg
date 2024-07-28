77c1541822b4dab548b8b04d1e106250a833f720
Brett DeWitt <bdewitt1984@gmail.com>
2024-07-28 14:26:33 -0700

    Added serialization methods from_dict(), from_json(), to_dict(), to_json(). Changed default name to self.__class__.__name__ to make serialization/deserialization easier.

--- source/api/base_api.py
+++ source/api/base_api.py
@@ -1,5 +1,10 @@
 # source/api/base_api.py
 
+"""
+    Base class for the plugin interface. Plugins must inherit from this class
+    in order to be detected by APIManager and used by Collection.
+"""
+import json
 # Standard library
 from abc import ABC, abstractmethod
 
@@ -7,11 +12,22 @@ from abc import ABC, abstractmethod
 # class BaseAPI(ABC, metaclass=MetaAPI):
 class BaseAPI(ABC):
     def __init__(self, name=None):
-        self.name = name
+        if name is None:
+            self.name = self.__class__.__name__
+        else:
+            self.name = name
 
     @abstractmethod
     def fetch_video_data(self, **kwargs):
         raise NotImplementedError("This function must be implemented in a subclass")
+
+    @classmethod
+    def from_dict(cls, d):
+        return cls(**d)
+
+    @classmethod
+    def from_json(cls, j):
+        return cls.from_dict(json.loads(j))
 
     def get_name(self):
         return self.name
@@ -23,3 +39,11 @@ class BaseAPI(ABC):
     @abstractmethod
     def get_required_params(self):
         raise NotImplementedError("This function must be implemented in a subclass")
+
+    @classmethod
+    def to_dict(cls):
+        return {'name': cls.__name__}
+
+    @classmethod
+    def to_json(cls):
+        json.dumps(cls.to_dict())
