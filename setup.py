8b3e5aa55c39e5f2c3a24ee3e0733bc4ab6ffd24
Brett DeWitt <bdewitt1984@gmail.com>
2024-07-28 14:15:08 -0700

    Added stub and example code for setup script

---
+++ setup.py
@@ -0,0 +1,23 @@
+# ./setup.py
+
+# Standard library
+
+# Local imports
+
+# Third-party packages
+from setuptools import setup, find_packages
+
+
+# setup(
+#     name='pyvorg',
+#     version='0.1',
+#     packages=find_packages(),
+#     install_requires=[
+#         # List your dependencies here
+#     ],
+#     entry_points={
+#         'console_scripts': [
+#             'video-manager=main:main',
+#         ],
+#     },
+# )
