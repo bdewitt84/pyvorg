899bb0bddecfe6ad8cb7a6c2a3a86ef7dd6be0dc
Brett DeWitt <bdewitt1984@gmail.com>
2024-07-28 19:22:17 -0700

    Moved the SessionManager instance to main.py for now, since it does not belong in ui.handle_parsed_args()

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ import sys
 
 # Local imports
 from source.cli.ui import parse_args, handle_parsed_args
+from source.session.session_man import SessionManager
 
 # Third-party packages
 
 
 if __name__ == "__main__":
+    session = SessionManager.unpickle_session()
     args = parse_args(sys.argv[1:])
-    handle_parsed_args(args)
+    handle_parsed_args(args, session)
+    session.pickle_session()
