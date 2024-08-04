832fae4ca2e84cdadbad3158cba13c205784e128
Brett DeWitt <bdewitt1984@gmail.com>
2024-08-04 15:15:05 -0700

    Initial commit of Filter class. Used by Collection to filter a subset of videos from a list.

---
+++ source/filter.py
@@ -0,0 +1,74 @@
+# ./source/utils/filter.py
+
+# Standard library
+import re
+
+# Local imports
+
+# Third-party packages
+
+
+class Filter:
+    def __init__(self, key, operator, right_operand):
+        self.key = key
+        self.operator = operator
+        self.right_operand = self.infer_value(right_operand)
+
+    def matches(self, left_operand):
+        inferred_value = self.infer_value(left_operand)
+        if isinstance(inferred_value, (int, float)):
+            return self.compare_numeric(inferred_value, self.operator, self.right_operand)
+        elif isinstance(inferred_value, str):
+            return self.compare_lexical(inferred_value, self.operator, self.right_operand)
+
+    @staticmethod
+    def from_string(string):
+        key, operator, value = Filter.parse_filter_string(string)
+        return Filter(key, operator, value)
+
+    @staticmethod
+    def infer_value(string):
+        try:
+            return float(string)
+        except ValueError:
+            pass
+
+        match = re.match(r"\d+", string)
+        if match:
+            return float(match.group())
+        else:
+            return string
+
+    @staticmethod
+    def parse_filter_string(string):
+        match = re.match(r"(\w+)([<>=])(.+)", string)
+        key, operator, value = match.groups()
+        return key.lower(), operator, value.lower()
+
+    @staticmethod
+    def compare_numeric(left, operator, right):
+        if operator == "=":
+            return left == right
+
+        elif operator == "<":
+            return left < right
+
+        elif operator == ">":
+            return left > right
+
+        else:
+            raise ValueError("Unknown operator")
+
+    @staticmethod
+    def compare_lexical(left, operator, right):
+        if operator == "=":
+            return left.lower() == right.lower()
+
+        elif operator == "<":
+            return left.lower() < right.lower()
+
+        elif operator == ">":
+            return left.lower() > right.lower()
+
+        else:
+            raise ValueError("Unknown operator")
