936f9f2bda927f5af1241823c3b01dcc13406316
Brett DeWitt <bdewitt1984@gmail.com>
2024-08-04 19:10:40 -0700

    Housekeeping; alphabetized methods

--- source/filter.py
+++ source/filter.py
@@ -14,12 +14,33 @@ class Filter:
         self.operator = operator
         self.right_operand = self.infer_value(right_operand)
 
-    def matches(self, left_operand):
-        inferred_value = self.infer_value(left_operand)
-        if isinstance(inferred_value, (int, float)):
-            return self.compare_numeric(inferred_value, self.operator, self.right_operand)
-        elif isinstance(inferred_value, str):
-            return self.compare_lexical(inferred_value, self.operator, self.right_operand)
+    @staticmethod
+    def compare_lexical(left, operator, right):
+        if operator == "=":
+            return left.lower() == right.lower()
+
+        elif operator == "<":
+            return left.lower() < right.lower()
+
+        elif operator == ">":
+            return left.lower() > right.lower()
+
+        else:
+            raise ValueError("Unknown operator")
+
+    @staticmethod
+    def compare_numeric(left, operator, right):
+        if operator == "=":
+            return left == right
+
+        elif operator == "<":
+            return left < right
+
+        elif operator == ">":
+            return left > right
+
+        else:
+            raise ValueError("Unknown operator")
 
     @staticmethod
     def from_string(string):
@@ -39,36 +60,19 @@ class Filter:
         else:
             return string
 
+    def matches(self, left_operand):
+        inferred_value = self.infer_value(left_operand)
+        if isinstance(inferred_value, (int, float)):
+            return self.compare_numeric(inferred_value, self.operator, self.right_operand)
+        elif isinstance(inferred_value, str):
+            return self.compare_lexical(inferred_value, self.operator, self.right_operand)
+
     @staticmethod
     def parse_filter_string(string):
         match = re.match(r"(\w+)([<>=])(.+)", string)
+
+        if match is None:
+            raise ValueError("Invalid filter string")
+
         key, operator, value = match.groups()
         return key.lower(), operator, value.lower()
-
-    @staticmethod
-    def compare_numeric(left, operator, right):
-        if operator == "=":
-            return left == right
-
-        elif operator == "<":
-            return left < right
-
-        elif operator == ">":
-            return left > right
-
-        else:
-            raise ValueError("Unknown operator")
-
-    @staticmethod
-    def compare_lexical(left, operator, right):
-        if operator == "=":
-            return left.lower() == right.lower()
-
-        elif operator == "<":
-            return left.lower() < right.lower()
-
-        elif operator == ">":
-            return left.lower() > right.lower()
-
-        else:
-            raise ValueError("Unknown operator")
