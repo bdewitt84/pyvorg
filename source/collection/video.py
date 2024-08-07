acd3c43ce8a7616c159ea8d0bed900292ed53775
Brett DeWitt <bdewitt1984@gmail.com>
2024-08-07 13:58:58 -0700

    Added typehint to path parameter to facilitate change from str to Path objects

--- source/collection/video.py
+++ source/collection/video.py
@@ -19,8 +19,7 @@ from utils.helper import timestamp_generate, hash_sha256
 
 
 class Video:
-    def __init__(self, path=None):
-        # TODO: Use Path objects
+    def __init__(self, path: Path = None):
         self.data = {USER_DATA: {}}
         if path is not None:
             self.update_file_data(path)
