62cfc6839a5de435b7de6b54161c97d9bb1744dc
Brett DeWitt <bdewitt1984@gmail.com>
2024-08-08 19:43:08 -0700

    Complete rewrite of test_helper to accommodate switch from path strings to pathlib Path objects

--- tests/test_utils/test_helper.py
+++ tests/test_utils/test_helper.py
@@ -4,83 +4,73 @@
 Unit tests for source/helper.py
 """
 # Standard library
-import tempfile
-import unittest
+from tempfile import TemporaryDirectory
+from unittest import TestCase
 
 # Local imports
 from utils.helper import *
 
 
-class TestClassName(unittest.TestCase):
+class TestHelper(TestCase):
+    def setUp(self) -> None:
+        self.temp_dir = TemporaryDirectory()
+        self.test_data = 'test data'
+
+    def tearDown(self) -> None:
+        self.temp_dir.cleanup()
 
     def test_class_name(self):
+        # Arrange
         integer = 1
         string = 'string'
         boolean = True
         none = None
 
+        # Act and Assert
         self.assertEqual('int', class_name(integer))
         self.assertEqual('str', class_name(string))
         self.assertEqual('bool', class_name(boolean))
         self.assertEqual('NoneType', class_name(none))
 
+    def test_hash_sha256(self):
+        # Arrange
+        filename = 'test.file'
+        file_path = Path(self.temp_dir.name) / filename
+        with file_path.open('wb') as file:
+            file.write(b'test_data')
 
-class HashSHA256(unittest.TestCase):
+        # Act
+        computed_hash = hash_sha256(file_path)
 
-    def test_hash_sha256_valid_file(self):
-        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
-            temp_file.write(b'test_data')
-            temp_file.close()
+        # Assert
+        expected_hash = 'e7d87b738825c33824cf3fd32b7314161fc8c425129163ff5e7260fc7288da36'
+        self.assertEqual(expected_hash, computed_hash)
 
-            expected_hash = 'e7d87b738825c33824cf3fd32b7314161fc8c425129163ff5e7260fc7288da36'
-            computed_hash = hash_sha256(temp_file.name)
-
-            os.remove(temp_file.name)
-
-            self.assertEqual(expected_hash, computed_hash)
-
-    def test_hash_sha256_file_not_found(self):
         with self.assertRaises(FileNotFoundError):
             hash_sha256('bogus_file_name')
 
-
-class TestTimestampValidate(unittest.TestCase):
-
-    def test_timestamp_validate_valid(self):
+    def test_timestamp_validate(self):
+        # Arrange
         valid_timestamp = '2000-01-01 01:00:00'
-        self.assertTrue(timestamp_validate(valid_timestamp))
-
-    def test_timestamp_validate_invalid_format(self):
         invalid_timestamp = '01:00:00 2000-01-01'
+
+        # Act and assert
+        self.assertTrue(timestamp_validate(valid_timestamp))
         self.assertFalse(timestamp_validate(invalid_timestamp))
 
+    def test_file_write(self):
+        # Arrange
+        write_ok = Path(self.temp_dir.name) / 'write_okay.file'
+        already_exists = Path(self.temp_dir.name) / 'already_exists.file'
+        with already_exists.open('w'):
+            pass
 
-class TestFileWrite(unittest.TestCase):
-
-    def setUp(self) -> None:
-        self.dir = tempfile.TemporaryDirectory()
-        self.test_data = 'test data'
-
-    def tearDown(self) -> None:
-        self.dir.cleanup()
-
-    def test_file_write_valid(self):
-        filename = 'doesnt_exist'
-        path = os.path.join(self.dir.name, filename)
-        file_write(path, self.test_data)
-
-        self.assertTrue(os.path.exists(path))
-        with open(path, 'r') as file:
-            written = file.read()
-        self.assertEqual(written, self.test_data)
-
-    def test_file_write_file_exists(self):
-        filename = 'exists'
-        path = os.path.join(self.dir.name, filename)
-        if os.path.exists(path):
-            raise FileExistsError(f"The file '{path}' already exists. Test file cannot be written")
-        with open(path, 'w') as file:
-            file.write(self.test_data)
+        # Act and Assert
+        file_write(write_ok, self.test_data)
+        self.assertTrue(write_ok.exists())
+        with open(write_ok, 'r') as file:
+            written_data = file.read()
+        self.assertEqual(written_data, self.test_data)
 
         with self.assertRaises(FileExistsError):
-            file_write(path, self.test_data)
+            file_write(already_exists, self.test_data)
