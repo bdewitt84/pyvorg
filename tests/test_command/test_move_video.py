f692968257b2bf51cfbe271428b0eed8f2e03b8b
Brett DeWitt <bdewitt1984@gmail.com>
2024-08-08 18:47:59 -0700

    Complete rewrite of unit tests for MoveVideo. Tests are now clearer, more organized, and isolated with mocks

--- tests/test_command/test_move_video.py
+++ tests/test_command/test_move_video.py
@@ -5,15 +5,13 @@
 """
 
 # Standard library
-import shutil
+from pathlib import Path
 import tempfile
 import unittest
-from unittest.mock import patch
+from unittest.mock import call, Mock, patch
 
 # Local imports
-from source.constants import *
-from source.command.move_video import *
-from collection.video import Video
+from source.command.move_video import MoveVideo
 
 # Third-party packages
 
@@ -22,149 +20,150 @@ class TestMoveVideoCommand(unittest.TestCase):
 
     def setUp(self) -> None:
         # Set up temp directory structure
-        self.src_dir = tempfile.TemporaryDirectory()
-        self.dest_dir = os.path.join(self.src_dir.name, 'dest')
-        os.makedirs(self.dest_dir)
+        self.temp_dir = tempfile.TemporaryDirectory()
+        self.src_dir = Path(self.temp_dir.name)
+        self.dest_dir = Path(self.src_dir) / 'dest'
+        self.dest_dir.mkdir()
 
         # Create temp video file
         self.filename = 'tempfile.mp4'
-        self.src_file_path = os.path.join(self.src_dir.name, self.filename)
-        self.test_file = open(self.src_file_path, 'w')
-        self.test_file.close()
+        self.src_file_path = Path(self.temp_dir.name) / self.filename
+        with self.src_file_path.open('w') as file:
+            file.write('dummy data')
 
         # Create temp video object
-        self.vid = Video()
+        self.vid = Mock()
         self.vid.data = {
             "file_data": {
                 "path": self.src_file_path,
-                "root": self.src_dir.name,
+                "root": self.temp_dir.name,
                 "filename": self.filename
             }
         }
+        self.vid.get_filename.return_value = self.filename
+        self.vid.get_path.return_value = self.src_dir / self.filename
+        self.vid.get_root.return_value = self.src_dir
 
         # Create MoveCommand object
-        self.cmd = MoveVideo(self.vid, self.dest_dir)
+        self.test_cmd = MoveVideo(self.vid, self.dest_dir)
 
     def tearDown(self) -> None:
-        self.src_dir.cleanup()
+        self.temp_dir.cleanup()
 
-    def test_exec(self):
-        expected_path = os.path.join(self.dest_dir, self.filename)
-        self.cmd.exec()
-        actual_path = self.vid.get_path()
-        self.assertTrue(os.path.exists(actual_path))
-        self.assertEqual(expected_path, actual_path)
+    @patch('source.command.move_video.MoveVideo._move')
+    def test_exec(self, mock_move):
+        # Act
+        self.test_cmd.exec()
 
-    def setUp_undo(self):
-        self.cmd.undo_dir = self.src_dir.name
-        self.cmd.created_dirs.append(self.dest_dir)
-        self.moved_path = shutil.move(self.test_file.name, self.dest_dir)
-        self.vid.data = {
-            "file_data": {
-                "path": self.moved_path,
-                "root": self.dest_dir,
-                "filename": self.filename
-            }
-        }
+        # Assert
+        self.assertEqual(self.test_cmd.origin_dir, self.src_dir)
+        mock_move.assert_called_once_with(self.dest_dir)
 
-    def test_undo(self):
-        expected_file_path = self.src_file_path
-        self.setUp_undo()
-        self.cmd.undo()
-        self.assertTrue(os.path.exists(expected_file_path))
-        self.assertEqual(expected_file_path, self.vid.get_path())
-        self.assertFalse(os.path.exists(self.dest_dir))
+    @patch('source.command.move_video.move_file')
+    @patch('source.command.move_video.MoveVideo._make_dirs')
+    def test_move(self, mock_make_dirs, mock_move_file):
+        # Arrange
+        dest_path = self.dest_dir / self.filename
 
-    def test_validate_exec_true(self):
-        valid, _ = self.cmd.validate_exec()
-        self.assertTrue(valid)
+        # Act
+        self.test_cmd._move(self.dest_dir)
 
-    def test_validate_exec_src_does_not_exist(self):
-        self.vid.data[FILE_DATA].update(
-            {"path": os.path.join(self.src_dir.name, 'does_not_exist.file')}
-        )
-        valid, msg = self.cmd.validate_exec()
-        print(msg)
-        self.assertFalse(valid)
+        # Assert
+        mock_make_dirs.assert_called_once_with(self.dest_dir)
+        mock_move_file.assert_called_once_with(self.vid.get_path(), dest_path)
+        self.vid.update_file_data.assert_called_once_with(dest_path)
 
-    def test_validate_exec_dst_already_exists(self):
-        exists_file = os.path.join(self.dest_dir, self.vid.data[FILE_DATA][FILENAME])
-        with open(exists_file, 'w'):
-            valid, msg = self.cmd.validate_exec()
-            print(msg)
-        self.assertFalse(valid)
+    @patch('source.command.move_video.MoveVideo._undo_make_dirs')
+    @patch('source.command.move_video.MoveVideo._move')
+    def test_undo(self, mock_move, mock_undo_make_dirs):
+        # Arrange
+        self.test_cmd.origin_dir = 'fake_path'
 
-    @patch('os.access')
-    def test_validate_exec_src_no_read(self, mock_access):
-        mock_access.side_effect = lambda path, mode: mode != os.R_OK
-        valid, msg = self.cmd.validate_exec()
-        print(msg)
-        self.assertFalse(valid, "Source file should not have read permission")
-        self.assertTrue(any('read' in m for m in msg), "'read' does not appear in the error message")
+        # Act
+        self.test_cmd.undo()
 
-    @patch('os.access')
-    def test_validate_exec_src_no_write(self, mock_access):
-        mock_access.side_effect = lambda path, mode: False if (path == self.src_file_path and mode == os.W_OK) else True
-        valid, msg = self.cmd.validate_exec()
-        print(msg)
-        self.assertFalse(valid, "Source file should not have write permission")
-        self.assertTrue(any('write' in m for m in msg))
+        # Assert
+        mock_move.assert_called_once_with('fake_path')
+        mock_undo_make_dirs.assert_called_once()
 
-    @patch('os.access')
-    def test_validate_exec_dst_no_write(self, mock_access):
-        mock_access.side_effect = lambda path, mode: False if (path == self.dest_dir and mode == os.W_OK) else True
-        print('test: ' + self.dest_dir)
-        valid, msg = self.cmd.validate_exec()
-        print(msg)
-        self.assertFalse(valid, "Destination file should not have write permission")
-        self.assertTrue(any('write' in m for m in msg))
+    @patch('source.command.move_video.dir_is_empty')
+    def test_undo_make_dirs(self, mock_dir_is_empty):
+        # Arrange
+        empty_dir = Mock()
+        not_empty_dir = Mock()
+        doesnt_exist_dir = Mock()
 
-    def test_validate_undo_true(self):
-        self.setUp_undo()
-        valid, _ = self.cmd.validate_undo()
-        self.assertTrue(valid)
+        empty_dir.exists.return_value = True
+        not_empty_dir.exists.return_value = True
+        doesnt_exist_dir.exists.return_value = True
 
-    def test_validate_undo_does_not_exist(self):
-        self.setUp_undo()
-        self.vid.data[FILE_DATA].update(
-            {"path": os.path.join(self.src_dir.name, 'does_not_exist.file')}
-        )
-        valid, msg = self.cmd.validate_undo()
-        self.assertFalse(valid)
-        self.assertTrue(any('does not exist' in m for m in msg))
+        empty_dir.empty = True
+        not_empty_dir.empty = False
 
-    def test_validate_undo_already_exists(self):
-        self.setUp_undo()
-        with open(self.src_file_path, 'w'):
-            valid, msg = self.cmd.validate_undo()
-            print(msg)
-            self.assertFalse(valid)
-            self.assertTrue(any('already exists' in m for m in msg))
+        mock_dir_is_empty.side_effect = lambda x: True if x.empty else False
 
-    @patch('os.access')
-    def test_validate_undo_src_no_read(self, mock_access):
-        self.setUp_undo()
-        mock_access.side_effect = lambda path, mode: False if (path == self.moved_path and mode == os.R_OK) else True
-        valid, msg = self.cmd.validate_undo()
-        print(msg)
-        self.assertFalse(valid)
-        self.assertTrue(any('read' in m for m in msg))
+        self.test_cmd.created_dirs = [empty_dir, not_empty_dir, doesnt_exist_dir]
 
-    @patch('os.access')
-    def test_validate_undo_src_no_write(self, mock_access):
-        self.setUp_undo()
-        mock_access.side_effect = lambda path, mode: False if (path == self.moved_path and mode == os.W_OK) else True
-        valid, msg = self.cmd.validate_undo()
-        print(msg)
-        self.assertFalse(valid)
-        self.assertTrue(any('write' in m for m in msg))
+        # Act
+        self.test_cmd._undo_make_dirs()
 
-    @patch('os.access')
-    def test_validate_undo_dst_no_write(self, mock_access):
-        self.setUp_undo()
-        mock_access.side_effect = lambda path, mode: False if (path == self.src_dir.name and mode == os.W_OK) else True
-        print(self.src_dir.name)
-        valid, msg = self.cmd.validate_undo()
-        print(msg)
-        self.assertFalse(valid)
-        self.assertTrue(any('write' in m for m in msg))
+        # Assert
+        empty_dir.rmdir.assert_called_once()
+        not_empty_dir.rmdir.assert_not_called()
+        doesnt_exist_dir.remdir.assert_not_called()
+
+    @patch('source.command.move_video.MoveVideo._validate_move')
+    def test_validate_exec(self, mock_validate_move):
+        # Arrange
+        self.vid.get_filename.return_value = 'video.file'
+        self.vid.get_path.return_value = Path('current dir') / 'video.file'
+        self.test_cmd.dest_dir = Path('target dir')
+
+        # Act
+        self.test_cmd.validate_exec()
+
+        # Assert
+        src_path = Path('current dir') / 'video.file'
+        dest_path = Path('target dir') / 'video.file'
+        mock_validate_move.assert_called_once_with(src_path, dest_path)
+
+    @patch('source.command.move_video.MoveVideo._validate_move')
+    def test_validate_undo(self, mock_validate_move):
+        # Arrange
+        self.vid.get_filename.return_value = 'video.file'
+        self.vid.get_path.return_value = Path('current dir') / 'video.file'
+        self.test_cmd.origin_dir = 'original dir'
+
+        # Act
+        self.test_cmd.validate_undo()
+
+        # Assert
+        src_path = Path('current dir') / 'video.file'
+        dest_path = Path('original dir') / 'video.file'
+        mock_validate_move.assert_called_with(src_path, dest_path)
+
+    @patch('source.command.move_video.path_is_readable')
+    @patch('source.command.move_video.path_is_writable')
+    def test_validate_move(self, mock_path_is_writable, mock_path_is_readable):
+        # Notes: Mocks return True by default when used as a condition in an
+        #        if statement. This unit test doesn't test the false branch
+        #        of these statements since they only append strings to a list
+
+        # Arrange
+        src_path = Mock()
+        dest_dir = Mock()
+        dest_path = Mock()
+        dest_path.parent = dest_dir
+
+        # Act
+        result_valid, result_msg = self.test_cmd._validate_move(src_path, dest_path)
+
+        # Assert
+        src_path.exists.assert_called_once()
+        dest_path.exists.assert_called_once()
+        mock_path_is_readable.assert_called_once_with(src_path)
+        expected_calls_writable = [call(src_path), call(dest_dir)]
+        mock_path_is_writable.assert_has_calls(expected_calls_writable, any_order=True)
+
+        self.assertTrue(result_valid)
+        self.assertEqual(result_msg, [])
