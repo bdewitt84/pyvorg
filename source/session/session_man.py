f07bc70adf4b74763c3575d0c4712fc88750676c
Brett DeWitt <bdewitt1984@gmail.com>
2024-08-14 18:53:09 -0700

    Major refactoring; focus on separation of responsibilities, friendly abstractions; renamed class to PyvorgSession

--- source/session/session_man.py
+++ source/session/session_man.py
@@ -6,8 +6,8 @@
 """
 
 # Standard library
-import os
 from pathlib import Path
+from typing import Optional
 
 # Local imports
 from source.api.api_manager import APIManager
@@ -16,58 +16,52 @@ from source.command.combuffer import CommandBuffer
 from source.command.update_video_data import UpdateVideoData
 from source.command.move_video import MoveVideo
 
-from source.utils.helper import file_write
+from source.utils.helper import file_write, get_files_from_path, parse_glob_string, generate_destination_paths
 
 # Third-party packages
 # n\a
 
 
-class SessionManager:
-    def __init__(self, path='./session.json'):
-        self.apiman = APIManager()
-        self.cb = CommandBuffer()
-        self.col = Collection()
-        self.profile_path = path
+class PyvorgSession:
+    # Todo: Decouple components once we get things hammered out
+    def __init__(self):
+        self.api_manager = APIManager()
+        self.command_buffer = CommandBuffer()
+        self.collection = Collection()
+        # TODO: Decide whether this should be done by APIManager's constructor
+        self.api_manager.init_plugins()
 
-        self.apiman.init_plugins()
+    def clear_staged_operations(self) -> None:
+        self.command_buffer.clear_exec_buffer()
 
-    def clear_transaction(self):
-        self.cb.clear_exec_buffer()
+    def commit_staged_operations(self) -> None:
+        self.command_buffer.execute_cmd_buffer()
 
-    def commit_transaction(self):
-        self.cb.execute_cmd_buffer()
+    def export_collection_metadata(self, path: Path, filter_strings=None) -> None:
+        file_write(path, self.collection.to_json(filter_strings))
 
-    def export_collection_metadata(self, path, filter_strings=None):
-        file_write(path, self.col.to_json(filter_strings))
+    def get_transaction_preview(self) -> str:
+        return str(self.command_buffer)
 
-    def get_transaction_preview(self):
-        return self.cb.__str__()
+    def scan_files_in_path(self, path_string: str, recursive: bool = False) -> None:
+        root, glob_pattern = parse_glob_string(path_string)
+        files = get_files_from_path(root, recursive, glob_pattern)
+        self.collection.add_files(files)
 
-    def scan_glob(self, glob_string: Path):
-        root = glob_string.parent
-        glob = glob_string.name
-        self.col.scan_glob(root, glob)
+    def stage_organize_video_files(self, filter_strings: Optional[list[str]] = None) -> None:
+        videos = self.collection.get_videos(filter_strings)
+        paths = generate_destination_paths(videos)
+        params = zip(videos, paths)
+        # TODO: We should abstract this further so we can stage commands in one call to a CommandBuffer method
+        cmds = self.command_buffer.build_commands(MoveVideo, params)
+        self.command_buffer.stage_commands(cmds)
 
-    def scan_path(self, path_string):
-        if '*' in path_string:
-            self.scan_glob(Path(path_string))
-        else:
-            path_obj = Path(path_string).resolve()
-            self.col.scan_path(path_obj)
+    def stage_update_api_metadata(self, api_names: list[str], filter_strings: Optional[list[str]]) -> None:
+        videos = self.collection.get_videos(filter_strings)
+        apis = self.api_manager.get_apis(api_names)
+        params = zip(videos, apis)
+        cmds = self.command_buffer.build_commands(UpdateVideoData, params)
+        self.command_buffer.stage_commands(cmds)
 
-    def stage_organize_video_files(self, filter_strings=None):
-        videos = self.col.get_videos(filter_strings)
-        for video in videos:
-            dest = Path(os.getenv('DEST_PATH')) / video.generate_dir_name()
-            cmd = MoveVideo(video, dest)
-            self.cb.add_command(cmd)
-
-    def stage_update_api_metadata(self, api_name, filter_strings=None):
-        api = self.apiman.get_api(api_name)
-        videos = self.col.get_videos(filter_strings)
-        for video in videos:
-            cmd = UpdateVideoData(video, api)
-            self.cb.add_command(cmd)
-
-    def undo_transaction(self):
-        self.cb.execute_undo_buffer()
+    def undo_transaction(self) -> None:
+        self.command_buffer.execute_undo_buffer()
