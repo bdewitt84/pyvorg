2370e03ad98d9ae83bfbfadde2ddfdd1ae2c3e34
Brett DeWitt <bdewitt1984@gmail.com>
2024-08-15 21:25:00 -0700

    Fixed minor errors that broke tests as a result of refactoring the source, aded stubs for tests found to be missing

--- tests/test_utils/test_helper.py
+++ tests/test_utils/test_helper.py
@@ -48,77 +48,22 @@ class TestHelper(TestCase):
         result = [file for file in target_path.iterdir()]
         self.assertEqual(expected, result)
 
-    def test_hash_sha256(self):
+    def test_dir_is_empty(self):
+        # TODO: Implement
+        pass
+
+    def test_file_read(self):
         # Arrange
-        filename = 'test.file'
-        file_path = Path(self.temp_dir.name) / filename
-        with file_path.open('wb') as file:
-            file.write(b'test_data')
+        file_exists = Path(self.temp_dir.name) / 'exists.file'
+        file_does_not_exist = Path(self.temp_dir.name) / 'does_not_exist.file'
 
-        # Act
-        computed_hash = hash_sha256(file_path)
-
-        # Assert
-        expected_hash = 'e7d87b738825c33824cf3fd32b7314161fc8c425129163ff5e7260fc7288da36'
-        self.assertEqual(expected_hash, computed_hash)
-
-        with self.assertRaises(FileNotFoundError):
-            hash_sha256('bogus_file_name')  # type:ignore
-
-    def test_move_file(self):
-        # Arrange
-        src_exists_path = Path(self.temp_dir.name) / 'exists.file'
-        src_does_not_exist_path = Path(self.temp_dir.name) / 'does_not_exist.file'
-        with src_exists_path.open('w'):
-            pass
-        dst_path = Path(self.temp_dir.name) / 'dst_folder'
-
-        # Act
-        with self.assertRaises(FileNotFoundError):
-            move_file(src_does_not_exist_path, dst_path)
-
-        move_file(src_exists_path, dst_path)
-
-        # Assert
-        with self.assertRaises(FileExistsError):
-            move_file(src_exists_path, dst_path)
-
-        self.assertFalse(src_exists_path.exists())
-        self.assertTrue(dst_path.exists())
-
-    def test_path_is_readable(self):
-        # Arrange
-        readable_target_path = Path(self.temp_dir.name) / 'readable.file'
-        non_readable_target_path = Path(self.temp_dir.name) / 'non_readable.file'
-
-        readable_target_path.touch(444)
-        non_readable_target_path.touch(333)
-
-        # Act and Assert
-        self.assertTrue(path_is_readable(readable_target_path))
-        # This test will always fail on Windows
-        # self.assertFalse(path_is_readable(non_readable_target_path))
-
-    def test_path_is_writable(self):
-        # Arrange
-        writable_target_path = Path(self.temp_dir.name) / 'writable.file'
-        non_writable_target_path = Path(self.temp_dir.name) / 'non_writable.file'
-
-        writable_target_path.touch(222)
-        non_writable_target_path.touch(555)
-
-        # Act and Assert
-        self.assertTrue(path_is_writable(writable_target_path))
-        self.assertFalse(path_is_writable(non_writable_target_path))
-
-    def test_timestamp_validate(self):
-        # Arrange
-        valid_timestamp = '2000-01-01 01:00:00'
-        invalid_timestamp = '01:00:00 2000-01-01'
+        file_exists.write_text(self.test_data)
 
         # Act and assert
-        self.assertTrue(timestamp_validate(valid_timestamp))
-        self.assertFalse(timestamp_validate(invalid_timestamp))
+        file_read(file_exists)
+
+        with self.assertRaises(FileNotFoundError):
+            file_read(file_does_not_exist)
 
     def test_file_write(self):
         # Arrange
@@ -137,18 +82,38 @@ class TestHelper(TestCase):
         with self.assertRaises(FileExistsError):
             file_write(already_exists, self.test_data)
 
-    def test_file_read(self):
+    def test_fill_kwargs_from_metadata(self):
+        # TODO: Implement
+        pass
+
+    def test_find_missing_params(self):
+        # TODO: Implement
+        pass
+
+    def test_generate_destination_paths(self):
+        # TODO: Implement
+        pass
+
+    def test_hash_sha256(self):
         # Arrange
-        file_exists = Path(self.temp_dir.name) / 'exists.file'
-        file_does_not_exist = Path(self.temp_dir.name) / 'does_not_exist.file'
+        filename = 'test.file'
+        file_path = Path(self.temp_dir.name) / filename
+        with file_path.open('wb') as file:
+            file.write(b'test_data')
 
-        file_exists.write_text(self.test_data)
+        # Act
+        computed_hash = hash_sha256(file_path)
 
-        # Act and assert
-        file_read(file_exists)
+        # Assert
+        expected_hash = 'e7d87b738825c33824cf3fd32b7314161fc8c425129163ff5e7260fc7288da36'
+        self.assertEqual(expected_hash, computed_hash)
 
         with self.assertRaises(FileNotFoundError):
-            file_read(file_does_not_exist)
+            hash_sha256('bogus_file_name')  # type:ignore
+
+    def test_make_dir(self):
+        # TODO: Implement
+        pass
 
     def test_mimic_folder(self):
         # Arrange
@@ -187,3 +152,70 @@ class TestHelper(TestCase):
         src_tree_dirs = [Path(dirpath).relative_to(src_tree) for dirpath, _, _ in os.walk(src_tree)]
         dst_tree_dirs = [Path(dirpath).relative_to(dst_tree) for dirpath, _, _ in os.walk(dst_tree)]
         self.assertEqual(src_tree_dirs, dst_tree_dirs)
+
+    def test_move_file(self):
+        # Arrange
+        src_exists_path = Path(self.temp_dir.name) / 'exists.file'
+        src_does_not_exist_path = Path(self.temp_dir.name) / 'does_not_exist.file'
+        with src_exists_path.open('w'):
+            pass
+        dst_path = Path(self.temp_dir.name) / 'dst_folder'
+
+        # Act
+        with self.assertRaises(FileNotFoundError):
+            move_file(src_does_not_exist_path, dst_path)
+
+        move_file(src_exists_path, dst_path)
+
+        # Assert
+        with self.assertRaises(FileExistsError):
+            move_file(src_exists_path, dst_path)
+
+        self.assertFalse(src_exists_path.exists())
+        self.assertTrue(dst_path.exists())
+
+    def test_parse_glob_string(self):
+        # TODO: Implement
+        pass
+
+    def test_path_is_readable(self):
+        # Arrange
+        readable_target_path = Path(self.temp_dir.name) / 'readable.file'
+        non_readable_target_path = Path(self.temp_dir.name) / 'non_readable.file'
+
+        readable_target_path.touch(444)
+        non_readable_target_path.touch(333)
+
+        # Act and Assert
+        self.assertTrue(path_is_readable(readable_target_path))
+        # This test will always fail on Windows
+        # self.assertFalse(path_is_readable(non_readable_target_path))
+
+    def test_path_is_writable(self):
+        # Arrange
+        writable_target_path = Path(self.temp_dir.name) / 'writable.file'
+        non_writable_target_path = Path(self.temp_dir.name) / 'non_writable.file'
+
+        writable_target_path.touch(222)
+        non_writable_target_path.touch(555)
+
+        # Act and Assert
+        self.assertTrue(path_is_writable(writable_target_path))
+        self.assertFalse(path_is_writable(non_writable_target_path))
+
+    def test_timestamp_generate(self):
+        # TODO: Implement
+        pass
+
+    def test_timestamp_validate(self):
+        # Arrange
+        valid_timestamp = '2000-01-01 01:00:00'
+        invalid_timestamp = '01:00:00 2000-01-01'
+
+        # Act and assert
+        self.assertTrue(timestamp_validate(valid_timestamp))
+        self.assertFalse(timestamp_validate(invalid_timestamp))
+
+    def test_update_api_data(self):
+        # TODO: Implement
+        pass
