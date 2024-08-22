4561b0846c1364151ffa2f0352459fe25e8fcb13
Brett DeWitt <bdewitt1984@gmail.com>
2024-08-21 19:29:50 -0700

    Refactor; loops changed to comprehensions, type hinting

--- source/utils/pluginservice.py
+++ source/utils/pluginservice.py
@@ -4,7 +4,7 @@
 import importlib
 import inspect
 import pkgutil
-from typing import Optional, Type
+from typing import Optional, Typefrom types import ModuleType
 
 # Local imports
 from source.api.base_api import BaseAPI
@@ -13,42 +13,42 @@ import source.api
 # Third-party plugins
 
 
-def get_plugin_instance(api_name: str) -> Optional[BaseAPI]:
-    modules = discover_api_modules(package=source.api)
-    for mod_name, mod in modules.items():
-        for cls_name, cls in discover_api_classes(mod).items():
-            if cls_name == api_name:
-                return cls()
-
-    # Todo: consider raising here
-    return None
-
-
-def discover_api_modules(package) -> (str, Type):
-    """
-    Looks for api modules in ./soruce/api/
-
-    :returns: dict of modules ending in '_api.py' in ./source/api/
-    """
-    modules = pkgutil.iter_modules(package.__path__, package.__name__ + '.')
-    api_mods = {
-        name: importlib.import_module(name)
-        for finder, name, ispkg
-        in modules
-        if '_api' in name
-    }
-    return api_mods
-
-
 def discover_api_classes(api_mod) -> (str, Type[BaseAPI]):
-    """
-    :param api_mod: List of tuples in (str, module) format.
-    :return: List of classes that inherit from BaseAPI
-    """
-    api_classes = {
+    return {
         class_name: obj
         for class_name, obj
         in inspect.getmembers(api_mod, inspect.isclass)
         if issubclass(obj, BaseAPI) and obj is not BaseAPI
     }
-    return api_classes
+
+
+def discover_api_modules(package: ModuleType) -> (str, Type):
+    modules = pkgutil.iter_modules(package.__path__, package.__name__ + '.')
+    return {
+        name: importlib.import_module(name)
+        for _, name, _
+        in modules
+        if '_api' in name
+    }
+
+
+def discover_plugins(package: ModuleType):
+    modules = discover_api_modules(package)
+    return {
+        clsname: cls
+        for _, mod
+        in modules.items()
+        for clsname, cls
+        in discover_api_classes(mod).items()
+    }
+
+
+def get_plugin_instance(api_name: str) -> Optional[BaseAPI]:
+    # TODO: Consider raising if None
+    # TODO: need to request or pass the plugin package instead
+    plugin_class = discover_plugins(source.api).get(api_name)
+    return plugin_class()
+
+
+def get_required_params(api: BaseAPI):
+    return api.get_required_params()
