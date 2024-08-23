3fdf93782e6dc2bcc9fdf3ed10dc0d7d06f84855
Brett DeWitt <bdewitt1984@gmail.com>
2024-08-23 14:49:05 -0700

    Refactor; Pared down main.py until relevant features are added

--- main.py
+++ main.py
@@ -2,14 +2,12 @@
 import sys
 
 # Local imports
-from source.cli.ui import parse_args, handle_parsed_args
-from source.session.session_man import SessionManager
+from source.ui.cli import run
+from source.session.pyvorg_session import PyvorgSession
 
 # Third-party packages
 
 
 if __name__ == "__main__":
-    session = SessionManager.unpickle_session()
-    args = parse_args(sys.argv[1:])
-    handle_parsed_args(args, session)
-    session.pickle_session()
+    session = PyvorgSession()
+    run(sys.argv[1:], session)
