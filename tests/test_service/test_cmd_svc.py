028c802dfa13b4f3455345150fa6c7d9565e1cf0
Brett DeWitt <bdewitt1984@gmail.com>
2024-08-24 16:36:10 -0700

    Tests; Added test file and stubs for cmd_svc.py

---
+++ tests/test_service/test_cmd_svc.py
@@ -0,0 +1,72 @@
+# ./source/tests/test_service/test_cmd_svc.py
+
+"""
+    Unit tests for cmd_svc.py
+"""
+
+# Standard library
+from tempfile import TemporaryDirectory
+from unittest import TestCase
+from unittest.mock import call, patch, Mock
+
+# Local imports
+import source.service.cmd_svc as cmd_svc
+
+# Third-party packages
+# n/a
+
+
+class TestCmdSvc(TestCase):
+    def setUp(self) -> None:
+        pass
+
+    def tearDown(self) -> None:
+        pass
+
+    def clear_exec_buffer(self):
+        # Arrange
+        # Act
+        # Assert
+        pass
+
+    def execute_cmd_buffer(self):
+        # Arrange
+        # Act
+        # Assert
+        pass
+
+    def get_exec_preview(self):
+        # Arrange
+        # Act
+        # Assert
+        pass
+
+    def build_command(self):
+        # Arrange
+        # Act
+        # Assert
+        pass
+
+    def test_build_commands(self):
+        # Arrange
+        # Act
+        # Assert
+        pass
+
+    def get_command_from_name(self):
+        # Arrange
+        # Act
+        # Assert
+        pass
+
+    def test_stage_commands(self):
+        # Arrange
+        # Act
+        # Assert
+        pass
+
+    def test_execute_undo_buffer(self):
+        # Arrange
+        # Act
+        # Assert
+        pass
