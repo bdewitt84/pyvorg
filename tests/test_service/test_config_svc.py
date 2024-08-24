da0ba823970ae73d8ec7f075bd2ecc10e78759f2
Brett DeWitt <bdewitt1984@gmail.com>
2024-08-24 16:48:19 -0700

    Tests; Added unit test module and stubs for config_svc.py

---
+++ tests/test_service/test_config_svc.py
@@ -0,0 +1,65 @@
+# ./source/tests/test_service/test_config_svc.py
+
+"""
+    Unit tests for config_svc.py
+"""
+
+# Standard library
+from tempfile import TemporaryDirectory
+from unittest import TestCase
+from unittest.mock import call, patch, Mock
+
+# Local imports
+import source.service.cmd_svc as cmd_svc
+
+# Third-party packages
+# n/a
+
+
+def test_get_default_dst_tree():
+    # Arrange
+    # Act
+    # Assert
+    pass
+
+
+def test_get_default_format_str():
+    # Arrange
+    # Act
+    # Assert
+    pass
+
+
+def test_get_user_cache_dir():
+    # Arrange
+    # Act
+    # Assert
+    pass
+
+
+def test_get_user_config_dir():
+    # Arrange
+    # Act
+    # Assert
+    pass
+
+
+def test_get_user_export_dir():
+    # Arrange
+    # Act
+    # Assert
+    pass
+
+
+def test_get_user_profile_dir():
+    # Arrange
+    # Act
+    # Assert
+    pass
+
+
+def test_get_user_logs_dir():
+    # Arrange
+    # Act
+    # Assert
+    pass
