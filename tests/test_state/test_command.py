a0d47f29a7c46a79f6808da715d4d8663ce591a6
Brett DeWitt <bdewitt1984@gmail.com>
2024-08-24 20:20:16 -0700

    Tests; Added test_command.py with stubs for unit tests

---
+++ tests/test_state/test_command.py
@@ -0,0 +1,77 @@
+# tests/test_command.py
+
+"""
+    Unit tests for source/state/command.py
+"""
+
+# Standard library
+from unittest import TestCase
+from unittest.mock import Mock
+
+# Local imports
+from state.combuffer import *
+from test_state.shared import FauxCmd
+
+
+class TestCommand(TestCase):
+    def setUp(self) -> None:
+        pass
+
+    def tearDown(self) -> None:
+        pass
+
+    def test_exec(self):
+        # TODO: Implement
+        # Arrange
+        # Act
+        # Assert
+        pass
+
+    def test_from_dict(self):
+        # TODO: Implement
+        # Arrange
+        # Act
+        # Assert
+        pass
+
+    def test_from_json(self):
+        # TODO: Implement
+        # Arrange
+        # Act
+        # Assert
+        pass
+
+    def test_to_dict(self):
+        # TODO: Implement
+        # Arrange
+        # Act
+        # Assert
+        pass
+
+    def test_to_json(self):
+        # TODO: Implement
+        # Arrange
+        # Act
+        # Assert
+        pass
+
+    def test_validate_exec(self):
+        # TODO: Implement
+        # Arrange
+        # Act
+        # Assert
+        pass
+
+    def test_validate_undo(self):
+        # TODO: Implement
+        # Arrange
+        # Act
+        # Assert
+        pass
+
+    def test_undo(self):
+        # TODO: Implement
+        # Arrange
+        # Act
+        # Assert
+        pass
