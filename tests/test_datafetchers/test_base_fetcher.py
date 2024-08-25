5fe244ff1b95fcc199a68c9fca9681d2c63318f9
Brett DeWitt <bdewitt1984@gmail.com>
2024-08-24 20:05:55 -0700

    Tests; Added test_base_fetcher.py with stubs for unit tests

---
+++ tests/test_datafetchers/test_base_fetcher.py
@@ -0,0 +1,79 @@
+# tests/test_datafetchers/test_base_fetcher.py
+
+"""
+    Unit tests for base_fetcher.py
+"""
+
+# Standard library
+from unittest import TestCase
+from unittest.mock import Mock
+
+# Local imports
+from source.datafetchers.base_fetcher import DataFetcher
+
+# Third-party packages
+
+
+class TestGuessitAPI(TestCase):
+
+    def setUp(self) -> None:
+        pass
+
+    def tearDown(self) -> None:
+        pass
+
+    def test_fetch_data(self):
+        # TODO: Implement
+        # Arrange
+        # Act
+        # Assert
+        pass
+
+    def test_from_dict(self):
+        # TODO: Implement
+        # Arrange
+        # Act
+        # Assert
+        pass
+
+    def test_from_json(self):
+        # TODO: Implement
+        # Arrange
+        # Act
+        # Assert
+        pass
+
+    def test_get_name(self):
+        # TODO: Implement
+        # Arrange
+        # Act
+        # Assert
+        pass
+
+    def test_get_optional_params(self):
+        # TODO: Implement
+        # Arrange
+        # Act
+        # Assert
+        pass
+
+    def test_get_required_params(self):
+        # TODO: Implement
+        # Arrange
+        # Act
+        # Assert
+        pass
+
+    def test_to_dict(self):
+        # TODO: Implement
+        # Arrange
+        # Act
+        # Assert
+        pass
+
+    def test_to_json(self):
+        # TODO: Implement
+        # Arrange
+        # Act
+        # Assert
+        pass
