0b462046ae29b1aa7e65fc86183c0926326a7889
Brett DeWitt <bdewitt1984@gmail.com>
2024-08-25 18:43:00 -0700

    Tests; Implemented unit tests for base class Command

--- tests/test_state/test_command.py
+++ tests/test_state/test_command.py
@@ -6,72 +6,66 @@
 
 # Standard library
 from unittest import TestCase
-from unittest.mock import Mock
 
 # Local imports
 from state.combuffer import *
-from test_state.shared import FauxCmd
 
 
 class TestCommand(TestCase):
     def setUp(self) -> None:
-        pass
+        class SubClass(Command):
+            def exec(self):
+                super().exec()
+
+            def to_dict(self):
+                super().to_dict()
+
+            def validate_exec(self):
+                super().validate_exec()
+
+            def validate_undo(self):
+                super().validate_undo()
+
+            def undo(self):
+                super().undo()
+
+        self.SubClass = SubClass
+        self.subclass = SubClass()
 
     def tearDown(self) -> None:
         pass
 
     def test_exec(self):
-        # TODO: Implement
         # Arrange
         # Act
         # Assert
-        pass
-
-    def test_from_dict(self):
-        # TODO: Implement
-        # Arrange
-        # Act
-        # Assert
-        pass
-
-    def test_from_json(self):
-        # TODO: Implement
-        # Arrange
-        # Act
-        # Assert
-        pass
+        with self.assertRaises(NotImplementedError):
+            self.subclass.exec()
 
     def test_to_dict(self):
-        # TODO: Implement
         # Arrange
         # Act
         # Assert
-        pass
-
-    def test_to_json(self):
-        # TODO: Implement
-        # Arrange
-        # Act
-        # Assert
-        pass
+        with self.assertRaises(NotImplementedError):
+            self.subclass.to_dict()
 
     def test_validate_exec(self):
-        # TODO: Implement
         # Arrange
         # Act
         # Assert
-        pass
+        with self.assertRaises(NotImplementedError):
+            self.subclass.exec()
 
     def test_validate_undo(self):
-        # TODO: Implement
         # Arrange
         # Act
         # Assert
-        pass
+        with self.assertRaises(NotImplementedError):
+            self.subclass.validate_undo()
 
     def test_undo(self):
-        # TODO: Implement
         # Arrange
         # Act
         # Assert
-        pass
+        with self.assertRaises(NotImplementedError):
+            self.subclass.undo()
