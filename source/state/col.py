389ee7db9900359d28074d2a612d09eecfdd20b5
Brett DeWitt <bdewitt1984@gmail.com>
2024-09-08 20:42:52 -0700

    Housekeeping; removed unused commented out code

--- source/state/col.py
+++ source/state/col.py
@@ -56,15 +56,6 @@ class Collection:
         self.videos.update({video_id: video})
         return video_id
 
-    # @staticmethod
-    # def from_dict(source: dict):
-    #     # Todo: Validate input dictionary
-    #     new_col = Collection()
-    #     for nested_dictionary in source.values():
-    #         new_vid = Video.from_dict(nested_dictionary)
-    #         new_col.add_video_instance(new_vid)
-    #     return new_col
-
     @staticmethod
     def generate_video_id(video: Video):
         return video.get_hash()
