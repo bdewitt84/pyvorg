d29d3c4f189ac584d81087889fe0afd133287254
Brett DeWitt <bdewitt1984@gmail.com>
2024-09-09 15:02:27 -0700

    Comments; updated setup stub. This is a trivial commit to test a configuration change in the IDE related to git

--- setup.py
+++ setup.py
@@ -9,15 +9,20 @@ from setuptools import setup, find_packages
 
 
 # setup(
-#     name='pyvorg',
-#     version='0.1',
+#     name='Pyvorg',
+#     version='0.1.0',
 #     packages=find_packages(),
-#     install_requires=[
-#         # List your dependencies here
-#     ],
 #     entry_points={
 #         'console_scripts': [
-#             'video-manager=main:main',
+#             'pyvorg=your_package.main:main',  # This defines the CLI command
 #         ],
 #     },
+#     install_requires=[
+#         'requests',  # List all your dependencies here
+#     ],
+#     author='Brett DeWitt',
+#     author_email='bdewitt1984@gmail.com',
+#     description='A lightweight CLI based media organizer',
+#     # TODO: update this when we update our github page
+#     url='https://github.com/bdewitt84/py-video-organizer',
 # )
