41462678589d011226804f8410ac7824804f80d3
Brett DeWitt <bdewitt1984@gmail.com>
2024-09-12 15:21:52 -0700

    Fix; finalized setup.py. Project should be ready to install for testing on VM

--- setup.py
+++ setup.py
@@ -8,21 +8,30 @@
 from setuptools import setup, find_packages
 
 
-# setup(
-#     name='Pyvorg',
-#     version='0.1.0',
-#     packages=find_packages(),
-#     entry_points={
-#         'console_scripts': [
-#             'pyvorg=your_package.main:main',  # This defines the CLI command
-#         ],
-#     },
-#     install_requires=[
-#         'requests',  # List all your dependencies here
-#     ],
-#     author='Brett DeWitt',
-#     author_email='bdewitt1984@gmail.com',
-#     description='A lightweight CLI based media organizer',
-#     # TODO: update this when we update our github page
-#     url='https://github.com/bdewitt84/py-video-organizer',
-# )
+setup(
+    name='pyvorg',
+    version='0.1.0',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'pyvorg=your_package.main:main',  # This defines the CLI command
+        ],
+    },
+    install_requires=[
+        'colorlog',
+        'guessit',
+        'requests',
+        'setuptools',
+        'tqdm',
+    ],
+    author='Brett DeWitt',
+    author_email='bdewitt1984@gmail.com',
+    description='A lightweight CLI based media organizer',
+    url='https://github.com/bdewitt84/pyvorg',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
